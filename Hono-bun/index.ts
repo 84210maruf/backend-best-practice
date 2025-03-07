a45cd1496f546edcea73d5cd63a7d3824221cad7
maruf <maruf84210cse@gmail.com>
2025-03-08 02:09:52 +0600

    feat : simple hono server

--- Hono-bun/index.ts
+++ Hono-bun/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+
+const app = new Hono();
+
+
+app.get('/', (c) => c.json('hallow! simple server'))
+
+Bun.serve({
+    fetch : app.fetch,
+    port : process.env.PORT || 3000
+})
+
+
+console.log("Hello via Bun (simple server)!");
